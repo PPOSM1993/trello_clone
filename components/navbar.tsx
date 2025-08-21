25b92812e6304b8a8e4fb5f2941cee193a267981
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-08-21 17:07:25 -0400

    Se configura base de datos mas se crean componentes de frontend

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,90 @@
 "use client";
-import { Trello } from "lucide-react";
+import { ArrowRight, Trello } from "lucide-react";
+import { SignInButton, SignUpButton, UserButton, } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 
 export default function Navbar() {
-  return (
-    <>
-    <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
-        <div className="container mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
-            <div className="flex items-center space-x-2">
-                <Trello className="h-6 w-6 sm:h-8 text-blue-600"/>
-                <span className="text-xl sm:text-2xl font-bold text-gray-900">Trello Clone</span>
-            </div>
-        </div>
-    </header>
-    </>
-  )
+
+    const { isSignedIn, user } = useUser();
+    const pathname = usePathname()
+
+    const isDashboardPage = pathname === "/dashboard";
+    const isBoardPage = pathname.startsWith("/boards/");
+
+    if (isDashboardPage) {
+        return (
+            <>
+                <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
+                    <div className="container mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
+                        <div className="flex items-center space-x-2">
+                            <Trello className="h-6 w-6 sm:h-8 text-blue-600" />
+                            <span className="text-xl sm:text-2xl font-bold text-gray-900">Trello Clone</span>
+                        </div>
+                        <div className="flex items-center space-x-2 sm:span-x-4">
+                            <UserButton/>
+                        </div>
+                    </div>
+                </header>
+            </>
+        )
+
+    }
+
+    return (
+        <>
+            <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
+                <div className="container mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
+                    <div className="flex items-center space-x-2">
+                        <Trello className="h-6 w-6 sm:h-8 text-blue-600" />
+                        <span className="text-xl sm:text-2xl font-bold text-gray-900">Trello Clone</span>
+                    </div>
+                    <div className="flex items-center space-x-2 sm:span-x-4">
+                        {isSignedIn ? (
+                            <div
+                                className="flex flex-col sm:flex-row items-end sm:items-center space-y-1 sm:space-y-0 sm:space-x-4"
+                            >
+                                <span className="text-xs sm:text-sm text-gray-600 font-bold hidden sm:block">
+                                    Welcome, {user.firstName ?? user.emailAddresses[0].emailAddress}
+                                </span>
+
+                                <Link href="/dashboard">
+                                    <Button size="sm" className="text-xs sm:text-sm">
+                                        Go to Dashboard <ArrowRight />
+                                    </Button>
+                                </Link>
+
+
+                            </div>) : (
+                            <div>
+                                <SignInButton>
+                                    <Button
+                                        variant="ghost"
+                                        size="sm"
+                                        className="text-xs sm:text-sm">
+                                        Sign In
+                                    </Button>
+                                </SignInButton>
+
+                                <SignUpButton>
+                                    <Button
+                                        variant="ghost"
+                                        size="sm"
+                                        className="text-xs sm:text-sm">
+                                        Sign Up
+                                    </Button>
+                                </SignUpButton>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+
+
 }
 
